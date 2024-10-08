0d2012bf942e3cd38a7c668f76ed9e3123e77b17
PruthviLahare <142437821+PruthviLahare@users.noreply.github.com>
2024-10-08 14:13:54 +0530

    Update checkArmstrong.js

--- checkArmstrong.js
+++ checkArmstrong.js
@@ -1,5 +1,5 @@
-let num=99,rem=0,digits=0,sum=0;
-let isArmstrong=num , cube=1;
+let num=153,rem=0,digits=0,sum=0;
+let isArmstrong=num , exponent=1;
 
 while(num>0){
     rem=num%10;
@@ -12,10 +12,10 @@ num=isArmstrong;
 
 while(num>0){
               rem=num%10;
-              cube=rem**digits;
-              sum=cube+sum;
+              exponent=rem**digits;
+              sum=exponent+sum;
               num=(num-rem)/10;
-              cube=1;
+              exponent=1;
               
         
             }
